9281b6ed45d7b4e201f8928de2859d621ec55e3e
William Fontaine <william.fontaine0308@gmail.com>
2023-12-14 21:06:50 +0000

    images path

--- src/components/Header/Banner.tsx
+++ src/components/Header/Banner.tsx
@@ -1,19 +1,18 @@
 import './Banner.scss';
 
 function Banner() {
-  const bubbleUrl = new URL('/images/bubble.png', import.meta.url).href;
   return (
     <div className="banner">
       <div className="banner-content">
         <div className="firstname">
-        <img src={bubbleUrl}
-          alt="bubble"
-          className='bubble' />
-        <p className="firstname">William</p>
+          <img src="/images/bubble.png"
+            alt="bubble"
+            className='bubble' />
+          <p className="firstname">William</p>
         </div>
         <div className='info'>
-        <p className="lastname">Fontaine</p>
-        <p className="desc">Développeur full stack.</p>
+          <p className="lastname">Fontaine</p>
+          <p className="desc">Développeur full stack.</p>
         </div>
       </div>
     </div>
